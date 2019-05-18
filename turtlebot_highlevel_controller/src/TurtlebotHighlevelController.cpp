6eb39f60d8953e2f1e0a9722d261f4fbc4633e98
hekrause <hekrause@htwg-konstanz.de>
2019-05-18 17:09:36 +0200

    Task 1, 3 done.

--- turtlebot_highlevel_controller/src/TurtlebotHighlevelController.cpp
+++ turtlebot_highlevel_controller/src/TurtlebotHighlevelController.cpp
@@ -11,6 +11,7 @@ TurtlebotHighlevelController::TurtlebotHighlevelController(ros::NodeHandle& node
     ros::requestShutdown();
   }
   subscriber_ = nodeHandle_.subscribe(subscriberTopic_, 1, &TurtlebotHighlevelController::topicCallback, this);
+  publisher_ = nodeHandle_.advertise<geometry_msgs::Twist>(publisherTopic_, 1);
   ROS_INFO("Successfully launched node.");
 }
 
@@ -20,7 +21,8 @@ TurtlebotHighlevelController::~TurtlebotHighlevelController()
 
 bool TurtlebotHighlevelController::readParameters()
 {
-  if (!nodeHandle_.getParam("topic", subscriberTopic_)) return false;
+  if (!nodeHandle_.getParam("subsciber_topic", subscriberTopic_)) return false;
+  if (!nodeHandle_.getParam("publisher_topic", publisherTopic_)) return false;
   if (!nodeHandle_.getParam("queue_size", queueSize_)) return false;
   return true;
 }
@@ -28,8 +30,23 @@ bool TurtlebotHighlevelController::readParameters()
 void TurtlebotHighlevelController::topicCallback(const sensor_msgs::LaserScan& message)
 {
   std::vector<float> ranges = message.ranges;
+  std::vector<float> intensities = message.intensities;
+  float max_elem = *std::max_element(intensities.begin(), intensities.end());
   float min_elem = *std::min_element(ranges.begin(), ranges.end());
-  ROS_INFO("Min distance: %3.3f", min_elem);
+  ROS_INFO("Min distance: %3.3f with intensiti<: %3.3f", min_elem, max_elem);
+
+/*
+  if (max_elem < 1) {
+    geometry_msgs::Twist msg;
+    msg.linear.x = 0.0;
+    msg.linear.y = 0.0;
+    msg.linear.z = 0.0;
+    msg.angular.x = 1.0;
+    msg.angular.y = 1.0;
+    msg.angular.z = 1.0;
+    this->publisher_.publish(msg);
+  }
+*/
 }
 
-} /* namespace */
+}
