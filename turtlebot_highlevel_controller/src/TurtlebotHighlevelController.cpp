7adfdd230fa5a7382dfd3635639872dd58a2cc9d
hekrause <hekrause@htwg-konstanz.de>
2019-05-24 13:31:34 +0200

    Ready for tag.

--- turtlebot_highlevel_controller/src/TurtlebotHighlevelController.cpp
+++ turtlebot_highlevel_controller/src/TurtlebotHighlevelController.cpp
@@ -30,28 +30,40 @@ bool TurtlebotHighlevelController::readParameters()
   return true;
 }
 
-void TurtlebotHighlevelController::placeMarker(float angle, float dist)
+void TurtlebotHighlevelController::moveRobot(float lx, float ly, float lz, float ax, float ay, float az)
 {
-  uint32_t shape = visualization_msgs::Marker::CUBE;
+  geometry_msgs::Twist msg;
+  msg.linear.x = lx;
+  msg.linear.y = ly;
+  msg.linear.z = lz;
+  msg.angular.x = ax;
+  msg.angular.y = ay;
+  msg.angular.z = az;
+  this->publisher_.publish(msg);
+}
+
+void TurtlebotHighlevelController::placeMarker(std::string frame_id, float x, float y, float z)
+{
+  uint32_t shape = visualization_msgs::Marker::CYLINDER;
   visualization_msgs::Marker marker;
-  marker.header.frame_id = "/base_link";
+  marker.header.frame_id = frame_id;
   marker.header.stamp = ros::Time::now();
-  marker.ns = "basic_shapes";
+  marker.ns = "target_pillar";
   marker.id = 0;
   marker.type = shape;
   marker.action = visualization_msgs::Marker::ADD;
-  marker.pose.position.x = cos(angle) * dist;
-  marker.pose.position.y = sin(angle) * dist;
-  marker.pose.position.z = 0.0;
+  marker.pose.position.x = x;
+  marker.pose.position.y = y;
+  marker.pose.position.z = z;
   marker.pose.orientation.x = 0.0;
   marker.pose.orientation.y = 0.0;
   marker.pose.orientation.z = 0.0;
   marker.pose.orientation.w = 1.0;
-  marker.scale.x = 0.4;
-  marker.scale.y = 0.4;
-  marker.scale.z = 0.4;
-  marker.color.r = 0.0f;
-  marker.color.g = 1.0f;
+  marker.scale.x = 0.2;
+  marker.scale.y = 0.2;
+  marker.scale.z = 0.2;
+  marker.color.r = 1.0f;
+  marker.color.g = 0.5f;
   marker.color.b = 0.0f;
   marker.color.a = 1.0;
   marker.lifetime = ros::Duration();
@@ -62,14 +74,6 @@ void TurtlebotHighlevelController::topicCallback(const sensor_msgs::LaserScan& m
 {
   std::vector<float> ranges = message.ranges;
   std::vector<float> intensities = message.intensities;
-  //float max_elem = *std::max_element(intensities.begin(), intensities.end());
-  //float min_elem = *std::min_element(ranges.begin(), ranges.end());
-
-//  sensor_msgs::PointCloud cloud;
-  //projector_.projectLaser(message, cloud)
-
-
-
 
   float min_element = message.range_max;
   int min_index = 0;
@@ -82,39 +86,33 @@ void TurtlebotHighlevelController::topicCallback(const sensor_msgs::LaserScan& m
     }
   }
 
-  float angle_in_rad = message.angle_min + min_index * message.angle_increment;
-  ROS_INFO("%d, %3.3f", min_index, angle_in_rad);
-
-  geometry_msgs::Twist msg;
+  // Aufgabe 2,3,4
+  float angle = message.angle_min + min_index * message.angle_increment;
   if(min_element < 5.0)
   {
-    msg.linear.x = 0.1;
+    this->moveRobot(0.1, 0.0, 0.0, 0.0, 0.0, angle);
   } else {
-    msg.linear.x = 0.0;
+    this->moveRobot(0.0, 0.0, 0.0, 0.0, 0.0, angle);
   }
-  msg.linear.y = 0.0;
-  msg.linear.z = 0.0;
-  msg.angular.x = 0.0;
-  msg.angular.y = 0.0;
-  msg.angular.z = angle_in_rad;
-  this->publisher_.publish(msg);
 
-  placeMarker(angle_in_rad, min_element);
+  // Aufgabe 6.a
+  double x = cos(angle) * min_element;
+  double y = sin(angle) * min_element;
+  double z = 0.0;
+  placeMarker("base_laser_link", x, y, z);
 
-/*
-  geometry_msgs::TransformStamped transformStamped;
+  // Aufgabe 6.b
+  tf2::Vector3 point(x, y, z);
+  geometry_msgs::TransformStamped transform;
+  tf2::Stamped<tf2::Transform> tf2;
   try {
-    transformStamped = tfBuffer_.lookupTransform("odom", "base_laser_link", ros::Time(0));
+    transform = tfBuffer_.lookupTransform("odom", "base_laser_link", ros::Time(0));
+    tf2::convert(transform, tf2);
+    tf2::Vector3 new_point = tf2 * point;
+    placeMarker("odom", new_point[0], new_point[1], new_point[2]);
   } catch (tf2::TransformException &exception) {
     ROS_WARN("%s", exception.what());
   }
-
-  ROS_INFO("%3.3f", transformStamped.transform.translation.x);
-  ROS_INFO("%3.3f", transformStamped.transform.translation.y);
-  ROS_INFO("%3.3f\n", transformStamped.transform.translation.z);
-*/
-
-
 }
 
 }
