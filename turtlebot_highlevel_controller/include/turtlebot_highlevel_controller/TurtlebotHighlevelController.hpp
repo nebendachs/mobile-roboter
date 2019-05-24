6f822754a3831894a5b1cb9d016d2a2d268c42a2
hekrause <hekrause@htwg-konstanz.de>
2019-05-24 09:28:18 +0200

    Update .gitignore.

--- turtlebot_highlevel_controller/include/turtlebot_highlevel_controller/TurtlebotHighlevelController.hpp
+++ turtlebot_highlevel_controller/include/turtlebot_highlevel_controller/TurtlebotHighlevelController.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
-// ROS
 #include <ros/ros.h>
-#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
+#include <string>
+#include <visualization_msgs/Marker.h>
+//#include <laser_geometry/laser_geometry.h>
 
 namespace turtlebot_highlevel_controller {
 
@@ -15,13 +21,18 @@ class TurtlebotHighlevelController
 
  private:
   bool readParameters();
+  void placeMarker();
   void topicCallback(const sensor_msgs::LaserScan& message);
 
   ros::NodeHandle& nodeHandle_;
   ros::Subscriber subscriber_;
   ros::Publisher publisher_;
+  ros::Publisher marker_pub_;
   std::string subscriberTopic_;
   std::string publisherTopic_;
+  tf2_ros::Buffer tfBuffer_;
+  tf2_ros::TransformListener tfListener_;
+  //laser_geometry::LaserProjection projector_;
   int queueSize_;
 };
 
--- turtlebot_highlevel_controller/src/turtlebot_highlevel_controller_node.cpp
+++ turtlebot_highlevel_controller/src/turtlebot_highlevel_controller_node.cpp
@@ -6,6 +6,8 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "turtlebot_highlevel_controller");
   ros::NodeHandle nodeHandle("~");
 
+  //tf2_ros::Buffer tfBuffer;
+  //tf2_ros::TransformListener tfListener(tfBuffer);
   turtlebot_highlevel_controller::TurtlebotHighlevelController turtlebotHighlevelController(nodeHandle);
 
   ros::spin();
