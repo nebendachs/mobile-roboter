4e0bc357dc4ce9430ee12484cf3a36c32d12436c
hekrause <hekrause@htwg-konstanz.de>
2019-05-24 13:31:34 +0200

    Ready for tag.

--- turtlebot_highlevel_controller/include/turtlebot_highlevel_controller/TurtlebotHighlevelController.hpp
+++ turtlebot_highlevel_controller/include/turtlebot_highlevel_controller/TurtlebotHighlevelController.hpp
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
@@ -9,7 +10,6 @@
 #include <sensor_msgs/PointCloud.h>
 #include <string>
 #include <visualization_msgs/Marker.h>
-//#include <laser_geometry/laser_geometry.h>
 
 namespace turtlebot_highlevel_controller {
 
@@ -21,7 +21,8 @@ class TurtlebotHighlevelController
 
  private:
   bool readParameters();
-  void placeMarker(float angle, float dist);
+  void moveRobot(float lx, float ly, float lz, float ax, float ay, float az);
+  void placeMarker(std::string frame_id, float x, float y, float z);
   void topicCallback(const sensor_msgs::LaserScan& message);
 
   ros::NodeHandle& nodeHandle_;
@@ -32,8 +33,8 @@ class TurtlebotHighlevelController
   std::string publisherTopic_;
   tf2_ros::Buffer tfBuffer_;
   tf2_ros::TransformListener tfListener_;
-  //laser_geometry::LaserProjection projector_;
   int queueSize_;
+  int drive_speed_;
 };
 
 }
