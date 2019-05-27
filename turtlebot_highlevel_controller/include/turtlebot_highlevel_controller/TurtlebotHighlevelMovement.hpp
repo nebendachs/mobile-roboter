1c4a97322d4bded8cfc4d98687e2e026bcdabfdf
hekrause <hekrause@htwg-konstanz.de>
2019-05-27 13:47:39 +0200

    Task 1.a

---
+++ turtlebot_highlevel_controller/include/turtlebot_highlevel_controller/TurtlebotHighlevelMovement.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "turtlebot_highlevel_controller/TransportMessage.h"
+#include <string>
+#include <visualization_msgs/Marker.h>
+#include <math.h>
+
+namespace turtlebot_highlevel_controller {
+
+class TurtlebotHighlevelMovement
+{
+ public:
+  TurtlebotHighlevelMovement(ros::NodeHandle& nodeHandle);
+  virtual ~TurtlebotHighlevelMovement();
+
+ private:
+  bool readParameters();
+  void moveRobot(float lx, float ly, float lz, float ax, float ay, float az);
+  void placeMarker(std::string frame_id, float x, float y, float z);
+  void topicCallback(const turtlebot_highlevel_controller::TransportMessage& message);
+
+  ros::NodeHandle& nodeHandle_;
+  ros::Subscriber subscriber_;
+  ros::Publisher movementPublisher_;
+  ros::Publisher markerPublisher_;
+  std::string subscriberTopic_;
+  std::string movementTopic_;
+  tf2_ros::Buffer tfBuffer_;
+  tf2_ros::TransformListener tfListener_;
+};
+
+}
--- turtlebot_highlevel_movement/include/turtlebot_highlevel_movement/TurtlebotHighlevelMovement.hpp
+++ turtlebot_highlevel_movement/include/turtlebot_highlevel_movement/TurtlebotHighlevelMovement.hpp
@@ -2,39 +2,35 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
-#include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/PointCloud.h>
 #include <string>
 #include <visualization_msgs/Marker.h>
+#include "turtlebot_highlevel_movement/TransportMessage.h"
 
-namespace turtlebot_highlevel_controller {
+namespace turtlebot_highlevel_movement {
 
-class TurtlebotHighlevelController
+class TurtlebotHighlevelMovement
 {
  public:
-  TurtlebotHighlevelController(ros::NodeHandle& nodeHandle);
-  virtual ~TurtlebotHighlevelController();
+  TurtlebotHighlevelMovement(ros::NodeHandle& nodeHandle);
+  virtual ~TurtlebotHighlevelMovement();
 
  private:
   bool readParameters();
   void moveRobot(float lx, float ly, float lz, float ax, float ay, float az);
   void placeMarker(std::string frame_id, float x, float y, float z);
-  void topicCallback(const sensor_msgs::LaserScan& message);
+  void topicCallback(const turtlebot_highlevel_movement::TransportMessage& message);
 
   ros::NodeHandle& nodeHandle_;
   ros::Subscriber subscriber_;
-  ros::Publisher publisher_;
-  ros::Publisher marker_pub_;
+  ros::Publisher movementPublisher_;
+  ros::Publisher markerPublisher_;
   std::string subscriberTopic_;
-  std::string publisherTopic_;
+  std::string movementTopic_;
   tf2_ros::Buffer tfBuffer_;
   tf2_ros::TransformListener tfListener_;
-  int queueSize_;
-  int drive_speed_;
 };
 
 }
